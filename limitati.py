81778b8c0b43fc7fbea7e520e7d004754b65c1d4
P i n u r u n <81405677+pinurun@users.noreply.github.com>
2021-04-16 23:54:09 +0700

    Update limitati.py

--- limitati.py
+++ limitati.py
@@ -29,8 +29,8 @@ updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher 
 
 def Start(update, context):
-    data = '\nTinggalkan Pesan Anda Dengan Menggunakan Perintah \n• /msg <pesan> \n\ncontoh: /msg hai'
-    update.message.reply_text("Selamat Datang Di\nPɪɴᴜʀᴜɴ Lɪᴍɪᴛᴀᴛɪ ```" + data + "\n```", parse_mode=ParseMode.MARKDOWN)
+    data = 'Silahkan Tinggalkan Pesan Anda dan Gunakan Perintah : \n•/nmsg <pesan> \n\ncontoh: /msg hai'
+    update.message.reply_text("```\n" + data + "\n```", parse_mode=ParseMode.MARKDOWN)
 
 def Reply(update, context):
     msg = update.message.text
@@ -41,7 +41,7 @@ def SendToCreator(update, context):
     msg = update.effective_message
     text = update.effective_message.text
     frst = text.replace("/msg", "")
-    scn = frst.replace("Selamat Datang Di\nPɪɴᴜʀᴜɴ Lɪᴍɪᴛᴀᴛɪ\nTinggalkan Pesan Anda Dengan Menggunakan Perintah \n• /msg <pesan> \n\n", "")
+    scn = frst.replace("Silahkan Tinggalkan Pesan Anda dan Gunakan Perintah : \n•/msg <pesan> \n\n", "")
     gg = scn.replace("contoh: /msg hai", "")
     chat_id = update.effective_chat.id
     message = "*1 Pesan Baru dari* [{name}](tg://user?id={id})\n{message}".format(name=name, id=msg.from_user.id, message=gg)
